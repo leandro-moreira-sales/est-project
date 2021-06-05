acf082fd8cde4d73338274a202761095847e952d
leandro <leandro_moreira08@hotmail.com>
2021-06-05 20:02:36 -0300

    php code for send the form

---
+++ project/formulario.php
@@ -0,0 +1,27 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+$nome = addcslashes($_POST['nome']);
+$email = addcslashes($_POST['email']);
+$telefone = addcslashes($_POST['telefone']);
+
+$to = "leandro_moreira08@hotmail.com";
+$subject = "Teste - Landro Moreira Sales";
+$body = "Nome: ".$nome." "\n".
+        "Email: ".$email."\n".
+        "Telefone: ".$telefone.";
+
+$header = "From:skyfire08@hotmail.com"."\n"."Reply-To:".$email."\n"."X-Mailer:PHP/".phpversion();
+
+if(mail($to,$subject,$body,$header)){
+    echo("Email enviado com sucesso!");
+}else{
+    echo("Falha no envio!");
+}
+
+
+}
+
+
+?>
