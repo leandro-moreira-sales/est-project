2a4548bd6617a3ac74de9df0bceafb65227ecddb
leandro <leandro_moreira08@hotmail.com>
2021-06-05 21:05:08 -0300

    Header upgrade

--- project/formulario.php
+++ project/formulario.php
@@ -6,22 +6,18 @@ $nome = addcslashes($_POST['nome']);
 $email = addcslashes($_POST['email']);
 $telefone = addcslashes($_POST['telefone']);
 
-$to = "leandro_moreira08@hotmail.com";
+$to = ":rh@tinpix.com.br";
 $subject = "Teste - Landro Moreira Sales";
 $body = "Nome: ".$nome." "\n".
         "Email: ".$email."\n".
         "Telefone: ".$telefone.";
 
-$header = "From:skyfire08@hotmail.com"."\n"."Reply-To:".$email."\n"."X-Mailer:PHP/".phpversion();
+$header = "leandro_moreira08@hotmail.com"."\n"."Reply-To:".$email."\n"."X-Mailer:PHP/".phpversion();
 
 if(mail($to,$subject,$body,$header)){
     echo("Email enviado com sucesso!");
 }else{
     echo("Falha no envio!");
 }
-
-
 }
-
-
 ?>
